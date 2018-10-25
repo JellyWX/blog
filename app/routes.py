c42cb136b83bf7ed890995b138483a4728d2cb1a
jude-LafiteIII <judewrs@gmail.com>
2018-10-25 13:00:20 +0100

    soundfx help and more on the tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from app import app
 
 
@@ -6,6 +6,10 @@ from app import app
 def index():
     return render_template('index.html', title="Home")
 
-@app.route('/articles/<name>')
+@app.route('/articles/<name>/')
 def articles(name):
     return render_template('articles/{}.html'.format(name))
+
+@app.route('/articles/')
+def articles_re():
+    return redirect(url_for('index'))
