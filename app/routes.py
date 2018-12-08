161d47f65925d34da9e71d01e3ba2382c1de07af
jude-LafiteIII <judewrs@gmail.com>
2018-12-08 15:30:20 +0000

    404 page

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,24 @@ from flask import render_template, url_for, redirect
 from app import app
 
 
+@app.errorhandler(404)
+def e404(e):
+    return render_template('404.html', title='404')
+
 @app.route('/')
 def index():
     return render_template('index.html', title="Home")
 
+
 @app.route('/articles/<name>/')
 def articles(name):
-    return render_template('articles/{}.html'.format(name), title=name.replace('_', ' ').title())
+    try:
+        r = render_template('articles/{}.html'.format(name), title=name.replace('_', ' ').title())
+    except:
+        r = render_template('404.html', title='404')
+
+    return r
+
 
 @app.route('/articles/')
 def articles_re():
